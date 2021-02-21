792ba6055ad4756dc926b46c6c8d7307f73dc564
i3lTerra <danibeltrandrago@gmail.com>
2021-02-21 10:31:07 +0100

    First commit, added air_pollution.py

---
+++ README.md
@@ -0,0 +1,10 @@
+Webapges to inspect:
+    - https://aqicn.org/map/barcelona/
+    - https://com-shi-va.barcelona.cat/ca/transit
+    - https://opendata-ajuntament.barcelona.cat/data/ca/dataset?tags=Mapes+dades+ambientals
+
+
+Workflow:
+    · Python file / Jupyter Notebook to extract the data from the webpages ==> Quering && preprocessing
+    · DB / Json to store the data
+    · Flask API where to make the quering from GRAFANA
---
+++ air_pollution.py
@@ -0,0 +1,32 @@
+# /usr/env/python3
+# Webpage: https://aqicn.org/map/barcelona/
+
+import requests, datetime, json, os
+
+web = "https://api.waqi.info/mapq2/bounds"
+
+token = requests.post("https://api.waqi.info/api/token/")
+bounds = requests.post(web, data='{"bounds":"1.4089512396850925,41.264754153616664,2.7992653696700303,41.6224837053181","inc":"placeholders","viewer":"webgl","zoom":10.787931681032608}')
+cities = [{'idx':i['idx'], 'name':i['name']} for i in bounds.json()['data']]
+
+for city in cities:
+    data = requests.post("https://api.waqi.info/api/feed/@%s/aqi.json" % city['idx'], data="token=%s&id=%s" % (token, city['idx']))
+
+    json_name = 'air_pollution_%s.json' % datetime.datetime.now().strftime("%H%M%S%d%m%Y")
+    path='json/%s' % city['idx']
+
+    try:
+        os.mkdir(path)
+    except:
+        pass
+
+    with open('%s/%s' % (path, json_name), mode="w", encoding="utf-8") as f:
+        json.dump(data.json(), f, ensure_ascii=False, indent=4)
+
+
+with open("json/cities_idx.json", mode="w", encoding='utf-8') as f:
+    json.dump(cities, f, ensure_ascii=False, indent=4)
+
+
+
+# token = requests.post("https://api.waqi.info/api/token/").json()['rxs']['obs'][0]['msg']['token']
